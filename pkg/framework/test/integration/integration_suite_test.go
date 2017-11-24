e7bb1e8df96c48502045f284b247d691b4bd90f5
Gareth Smith <gsmith@pivotal.io>
2017-11-24 10:20:07 +0000

    Add fixtures tintegration tests
    Testing the lifecycle of our fixtures with the real binaries. Test if we
    can start the fixtures, the porcesses actually listen on the ports and
    if we tear down all the parts successfully again.

---
+++ pkg/framework/test/integration/integration_suite_test.go
@@ -0,0 +1,19 @@
+package integration_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+
+	"github.com/onsi/gomega/gexec"
+)
+
+func TestIntegration(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Integration Suite")
+}
+
+var _ = AfterSuite(func() {
+	gexec.TerminateAndWait()
+})
