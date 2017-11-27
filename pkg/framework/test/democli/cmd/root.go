92b0d330bd31ee92771a6987e100a43b6f15da47
Gareth Smith <gsmith@pivotal.io>
2017-11-27 20:49:01 +0000

    Add help text and barebones integration test
    We use [ginkgo](http://onsi.github.io/ginkgo/) and
    [gomega](http://onsi.github.io/gomega/) for testing. We generate some
    boilerplate with:
    ```
    mkdir integration
    cd integration
    ginkgo bootstrap
    ginkgo generate integration
    ```
    
    We use
    [gexec](http://onsi.github.io/gomega/#gexec-testing-external-processes)
    to compile and run the CLI under test, and to inspect its output.
    
    We use `dep ensure` to ensure that all our dependencies are properly
    vendored. From now on, this will be our workflow with every commit.

--- pkg/framework/test/democli/cmd/root.go
+++ pkg/framework/test/democli/cmd/root.go
@@ -28,13 +28,10 @@ var cfgFile string
 // RootCmd represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "democli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "A demo CLI application",
+	Long: `This is a demo kubernetes CLI, which interacts with the kubernetes API.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+The purpose of this CLI is to demo the testing framework that was used to develop it.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	//	Run: func(cmd *cobra.Command, args []string) { },
