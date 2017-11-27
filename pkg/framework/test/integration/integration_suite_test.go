20ea6213f8d1ea0a2253837cb50ad18c1e2389b0
Gareth Smith <gsmith@pivotal.io>
2017-11-27 17:42:32 +0000

    Track the time it takes to start&stop the fixtures

--- pkg/framework/test/integration/integration_suite_test.go
+++ pkg/framework/test/integration/integration_suite_test.go
@@ -6,6 +6,9 @@ import (
 
 	"testing"
 
+	"os"
+	"path/filepath"
+
 	"github.com/onsi/gomega/gexec"
 )
 
@@ -13,6 +16,19 @@ func TestIntegration(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Integration Suite")
 }
+
+var (
+	defaultPathToEtcd      string
+	defaultPathToApiserver string
+)
+
+var _ = BeforeSuite(func() {
+	assetsDir, ok := os.LookupEnv("KUBE_ASSETS_DIR")
+	Expect(ok).To(BeTrue(), "Expected $KUBE_ASSETS_DIR to be set")
+
+	defaultPathToEtcd = filepath.Join(assetsDir, "etcd")
+	defaultPathToApiserver = filepath.Join(assetsDir, "kube-apiserver")
+})
 
 var _ = AfterSuite(func() {
 	gexec.TerminateAndWait()
