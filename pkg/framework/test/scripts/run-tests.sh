63de385c65c5168f7dd4727e52905669b9d96d32
Hannes Hörl <hhorl@pivotal.io>
2017-11-28 12:25:39 +0000

    Add test option to run performance tests
    Performance tests are now skipped by default, but run in CI.

--- pkg/framework/test/scripts/run-tests.sh
+++ pkg/framework/test/scripts/run-tests.sh
@@ -4,11 +4,18 @@ set -eu
 # Use DEBUG=1 ./scripts/run-tests.sh to get debug output
 [[ -z "${DEBUG:-""}" ]] || set -x
 
-ginkgo_args=''
-[[ -z "${GINKGO_WATCH:-""}" ]] || ginkgo_args="${ginkgo_args} watch"
+declare -a ginkgo_args
+
+if [[ -n "${GINKGO_WATCH:-""}" ]] ; then
+  ginkgo_args=( "${ginkgo_args[@]}" "watch" )
+fi
+
+if [[ -z ${GINKGO_PERFORMANCE:-""} ]] ; then
+  ginkgo_args=( "${ginkgo_args[@]}" "-skipMeasurements" )
+fi
 
 test_framework_dir="$(cd "$(dirname "$0")/.." ; pwd)"
 
 export KUBE_ASSETS_DIR="${test_framework_dir}/assets/bin"
 
-ginkgo $ginkgo_args -r "${test_framework_dir}"
+ginkgo "${ginkgo_args[@]}" -r "${test_framework_dir}"
