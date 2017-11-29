bd2a451dc5898d09bce3b22cd3a3d00a146fe8b9
Hannes Hörl <hhorl@pivotal.io>
2017-11-29 15:53:06 +0000

    Make the framework test bash3 compatible

--- pkg/framework/test/scripts/run-tests.sh
+++ pkg/framework/test/scripts/run-tests.sh
@@ -4,18 +4,17 @@ set -eu
 # Use DEBUG=1 ./scripts/run-tests.sh to get debug output
 [[ -z "${DEBUG:-""}" ]] || set -x
 
-declare -a ginkgo_args
-
+GINKGO="ginkgo"
 if [[ -n "${GINKGO_WATCH:-""}" ]] ; then
-  ginkgo_args=( "${ginkgo_args[@]}" "watch" )
+  GINKGO="$GINKGO watch"
 fi
 
 if [[ -z ${GINKGO_PERFORMANCE:-""} ]] ; then
-  ginkgo_args=( "${ginkgo_args[@]}" "-skipMeasurements" )
+  GINKGO="$GINKGO -skipMeasurements"
 fi
 
 test_framework_dir="$(cd "$(dirname "$0")/.." ; pwd)"
 
 export KUBE_ASSETS_DIR="${test_framework_dir}/assets/bin"
 
-ginkgo "${ginkgo_args[@]}" -r "${test_framework_dir}"
+$GINKGO -r "${test_framework_dir}"
