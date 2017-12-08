6981a99ebf10d6e367adfdf18a1d079a72fc3af6
wackxu <xushiwei5@huawei.com>
2017-12-08 14:22:48 +0800

    update vendor

--- vendor/k8s.io/api/events/v1beta1/doc.go
+++ vendor/k8s.io/api/events/v1beta1/doc.go
@@ -14,8 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-// +k8s:deepcopy-gen=package,register
-// +k8s:openapi-gen=false
+// +k8s:deepcopy-gen=package
+// +k8s:openapi-gen=true
 
-// +groupName=admission.k8s.io
-package v1alpha1 // import "k8s.io/api/admission/v1alpha1"
+// +groupName=events.k8s.io
+package v1beta1 // import "k8s.io/api/events/v1beta1"
