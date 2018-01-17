24e13e5c03402831a4161fa70eb0ddb13cc1dd4d
Gareth Smith <gsmith@pivotal.io>
2018-01-17 11:10:19 +0000

    Update and add documentation

--- pkg/framework/test/etcd.go
+++ pkg/framework/test/etcd.go
@@ -10,20 +10,37 @@ import (
 )
 
 // Etcd knows how to run an etcd server.
-//
-// The documentation and examples for the Etcd's properties can be found in
-// in the documentation for the `APIServer`, as both implement a `ControlPaneProcess`.
 type Etcd struct {
-	URL          *url.URL
-	Path         string
-	DataDir      string
-	StopTimeout  time.Duration
+	// URL is the address the Etcd should listen on for client connections.
+	//
+	// If this is not specified, we default to a random free port on localhost.
+	URL *url.URL
+
+	// Path is the path to the etcd binary.
+	//
+	// If this is left as the empty string, we will attempt to locate a binary,
+	// by checking for the TEST_ASSET_ETCD environment variable, and
+	// the default test assets directory.
+	Path string
+
+	// DataDir is a path to a directory in which etcd can store its state.
+	//
+	// If left unspecified, then the Start() method will create a fresh temporary
+	// directory, and the Stop() method will clean it up.
+	DataDir string
+
+	// StartTimeout, StopTimeout specify the time the Etcd is allowed to
+	// take when starting and stopping before an error is emitted.
+	//
+	// If not specified, these default to 20 seconds.
 	StartTimeout time.Duration
+	StopTimeout  time.Duration
 
 	processState *internal.ProcessState
 }
 
-// Start starts the etcd, waits for it to come up, and returns an error, if occoured.
+// Start starts the etcd, waits for it to come up, and returns an error, if one
+// occoured.
 func (e *Etcd) Start() error {
 	var err error
 
@@ -48,7 +65,8 @@ func (e *Etcd) Start() error {
 	return e.processState.Start()
 }
 
-// Stop stops this process gracefully, waits for its termination, and cleans up the data directory.
+// Stop stops this process gracefully, waits for its termination, and cleans up
+// the DataDir if necessary.
 func (e *Etcd) Stop() error {
 	return e.processState.Stop()
 }
